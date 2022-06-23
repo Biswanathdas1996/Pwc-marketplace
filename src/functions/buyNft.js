512f4a8eedd884854e7113b87aebeda99e1b97e2
your_username <your_email_address@example.com>
2022-06-23 07:54:52 +0530

    ok

--- src/functions/buyNft.js
+++ src/functions/buyNft.js
@@ -1,37 +1,33 @@
-import { _fetch } from "../CONTRACT-ABI/connect";
+import { _fetch, _Walletaccount, _transction } from "../CONTRACT-ABI/connect";
 
-import {
-  _transction as transction,
-  _transction_signed,
-} from "../CONTRACT-ABI-ERC20/connect";
+import { _transction as transction } from "../CONTRACT-ABI-ERC20/connect";
 
 export const buyNft = async (tokenId) => {
   try {
     const price = await _fetch("getNftPrice", tokenId);
     console.log("===============", price);
-    // const owner = await _fetch("ownerOf", tokenId);
-    // const account = await _Walletaccount();
-    // const responseData = await _transction(
-    //   "buyNft",
-    //   owner,
-    //   account,
-    //   Number(tokenId)
+    const owner = await _fetch("ownerOf", tokenId);
+    const account = await _Walletaccount();
+
+    // await transction(
+    //   "approve",
+    //   "0xb78b8053B701a8e83bE191431533090A51fbF7ED",
+    //   price
     // );
 
-    await transction(
-      "approve",
-      "0xb78b8053B701a8e83bE191431533090A51fbF7ED",
-      price
-    );
+    const transFreFrom = await transction("transfer", owner, price);
 
-    const transFreFrom = await _transction_signed(
-      "transferFrom",
-      "0xfd55CC92d43C9d540975dD3C4177BbEf1015b902",
-      "0xb78b8053B701a8e83bE191431533090A51fbF7ED",
-      price
-    );
-
-    return transFreFrom;
+    if (transFreFrom?.error) {
+      return transFreFrom;
+    } else {
+      const transfredtxn = await _transction(
+        "buyNft",
+        owner,
+        account,
+        Number(tokenId)
+      );
+      return transfredtxn;
+    }
   } catch (error) {
     console.log("Error", error);
     return error;
